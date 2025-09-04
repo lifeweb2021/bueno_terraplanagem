ee8549e7ef3ae9cc93acfd31738982094dd1879d
Alexsandro Gomes Pereira <alexsandro.pereira@gmail.com>
2025-09-04 15:24:31 -0300

    Updated QuoteList.tsx

--- src/components/QuoteList.tsx
+++ src/components/QuoteList.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Quote, Order } from '../types';
 import { supabaseStorage } from '../utils/supabaseStorage';
+import {
   FileText, 
   Plus, 
   Search, 
