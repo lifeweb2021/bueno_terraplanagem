cab03614890a2558c6bc83c210ee5d36951e6e45
Alexsandro Gomes Pereira <alexsandro.pereira@gmail.com>
2025-09-04 00:27:52 -0300

    Updated OrderList.tsx

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,1 +1,25 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import React from "react";
+
+type Order = {
+  id: number;
+  item: string;
+  quantity: number;
+};
+
+interface OrderListProps {
+  orders: Order[];
+}
+
+const OrderList: React.FC<OrderListProps> = ({ orders }) => {
+  return (
+    <ul>
+      {orders.map((order) => (
+        <li key={order.id}>
+          {order.item} - {order.quantity}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default OrderList;
