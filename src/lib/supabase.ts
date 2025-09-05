b662ae02ff3e9334752f3282747ed20a3fca0aae
Alexsandro Gomes Pereira <alexsandro.pereira@gmail.com>
2025-09-04 21:14:28 -0300

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,15 +1,3 @@
-/*import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables');
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);*/
-
-// src/utils/supabaseStorage.ts
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 // Pegando variáveis de ambiente
